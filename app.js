32eb9abb6091c9cabc697b51634bf0bc4eb3e192
Emily Crepaldi <emily.ccarvalho13@gmail.com>
2023-10-11 17:10:16 -0300

    JS

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 'use strict'
 
-const header = [
-    {
-        h1: 'Tumblr-Cat',
-    }
-]
+async function pegarImagens() {
+    const url = 'https://api.thecatapi.com/v1/images/search?limit=03'
+    const response = await fetch(url)
+    const imagens = await response.json()
+    
+    return imagens.message
+}
+
+function criarImg(imagem) {
+    const card = document.getElementById('galeria')
+    const tagImg = document.createElement('img')
+    tagImg.src = imagem
+    galeria.appendChild(tagImg)
+
+}
+
+async function carregarGaleria() {
+    const imagens = await pegarImagens()
+    imagens.forEach(criarImg)
+}
+
+carregarGaleria()
